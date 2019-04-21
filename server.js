bd2c05e07d0f8b209fd81268b6151cd38bda35e2
chidha2005 <chidha2005@gmail.com>
2019-04-21 12:59:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -10,17 +10,12 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/login',(req,res) => {
-    var userName = req.body.userName,
-    var password = req.body.password
-    
-    if(userName === 'admin' && password === 'admin) {
-           res.json('{ 'status':'success'})
-
-       } else {
-          res.json('{ 'status':'failure'})
-
-
-       }
+    if(req.body.userName == 'admin' && req.body.password == 'admin') {
+        res.json("{'status':'success'}");
+    } else {
+        res.json("{'status':'failure'}");
+    }
+    res.json(req.body)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
