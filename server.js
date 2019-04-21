562329e5a8501d6a5647a27e90094e8eb735479c
chidha2005 <chidha2005@gmail.com>
2019-04-21 13:50:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -7,12 +8,48 @@ app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: false
   })); 
-app.post('/login',(req,res) => {
-    if(req.body.userName == 'admin' && req.body.password == 'admin') {
-        res.json("{'status':'success'}");
+
+  var URL ='mongodb+srv://admin:admin@being-zero-yduch.mongodb.net/test?retryWrites=true';
+
+mongoose.connect(URL, {useNewUrlParser: true});
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+console.log('connected to mongodb')}
+);
+
+var userSchema = new mongoose.Schema( { 
+    userName : String,
+    password: String
+})
+
+var userModel = mongoose.model('user',userSchema)
+
+app.post('/register',(req,res) => {
+   var user = new userModel({'userName':req.body.userName,'password':req.body.userName});
+   user.save((err,savedUser) => {
+       console.log('error ' +err);
+    if(err!=null) {
+        res.json("{status: 'Failure'}")
     } else {
-        res.json("{'status':'failure'}");
+        res.json("{status: 'Success'}")
     }
+   }) 
+})
+
+app.get('/users', function(req, resp){
+    userModel.find({}, function(arr, users) {
+        resp.json(users);
+     });
+});
+
+app.post('/login', function(req,res){
+   if(userModel.findOne(new userModel({'userName':req.body.userName,'password':req.body.userName}))) {
+       res.json("{'status':'Valid User'}");
+   } else{
+       res.json("{'status':'Invalid User' } ");
+   }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
